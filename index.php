a99041bfe8c2cbf3a35234d3283ed7e217abf251
ezkibel92 <ezequielsegundo20392@gmail.com>
2022-10-05 22:09:38 -0300

    prueba

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
    
     <form action="consultar.php" method="post">
 
-    <label for="cedula">Idngrese su CI sin puntos ni guiones</label>
+    <label for="cedula">Ingrese su CI sin puntos ni guiones</label>
     <input id="cedula" type="numer" name="cedula">
     <button for="cedula" type="submit">Ingresar</button>
     </form>
